3848e7aff289cc971aa648256b4a783a02164e1d
Day Little <dayana.y.little@gmail.com>
2023-02-28 14:27:56 -0500

    define associations

--- models/rating.js
+++ models/rating.js
@@ -4,14 +4,16 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Rating extends Model {
+  
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
-    }
+      Rating.belongsTo(models.Profile, { foreignKey: 'raterId' })
+      Rating.belongsTo(models.Profile, { foreignKey: 'profileId' })
+      }
   }
   Rating.init({
     value: {
