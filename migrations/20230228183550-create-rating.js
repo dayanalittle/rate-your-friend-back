21aa6d01d8e0382d3c83bcb0e000e602a4aea58d
Day Little <dayana.y.little@gmail.com>
2023-02-28 14:08:40 -0500

    update migrations

--- migrations/20230228183550-create-rating.js
+++ migrations/20230228183550-create-rating.js
@@ -10,14 +10,32 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       value: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        validate: {
+          min: 0,
+          max: 5,
+        },
       },
       profileId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Profiles',
+          key: 'id',
+        },
       },
-      raterId: {
-        type: Sequelize.INTEGER
+
+      voterId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Profiles',
+          key: 'id',
+        },
       },
+      
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
